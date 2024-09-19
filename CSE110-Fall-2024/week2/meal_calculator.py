387e306498fc4e8b4cccfe901bdec263987d76e1
Matthew Rosenthal <matthew2rosenthal@gmail.com>
2024-09-18 23:51:11 -0600

    meal update 2

--- CSE110-Fall-2024/week2/meal_calculator.py
+++ CSE110-Fall-2024/week2/meal_calculator.py
@@ -1,6 +1,9 @@
 #created by Matthew Rosenthal
 
 #get cost of meals from user
+from ast import Break
+
+
 adult_meal=float(input ('how much does the adult meal cost?: $'))
 kid_meal = float(input ('how much does the kid meal cost?: $'))
 
@@ -31,24 +34,35 @@ print(f'Tax: ${total_tax:.2f}')
 
 #display total due to user
 total = subtotal + total_tax
+print (f'Total: ${total:.2f}')
+cash = float(input('How much cash were you given?: '))
 
 #Cash and total 
-def change(c,t):
-    change = c - t
-    return change
-cash = float(input('How much cash were you given?: '))
-change_due = cash-total
+def change(cash,total):
+while True:
+    C=cash
+    T=total
+    change_due = C-T
+    if change_due>=0:
+        change=change_due
+        return print(f'The change due is:{change}')
+    if change>=0:
+        break
+    else:
+        temp=abs(change_due)
+        print(f'Still Needed: ${temp:.2f}')
+        more=float(input('How much more money was given?: '))
+        C+more
+     
+            
+    
 
-if change_due<0:
-    
-   new_t=abs(change)
-   print(f'The Customer owes: ${new_t:.2f}')
-    
-else:
-    print (f'Total: ${total:.2f}')
+
+
+
+# The line `print(change(cash,total))` is calling the `change` function with the arguments `cash`
+# and `total`, and then printing the return value of the function.
+change(cash,total) 
 
 print('-------------------------------------------------')
 
-
-
-print (f'the Change Due is: ${change_due:.2f}')

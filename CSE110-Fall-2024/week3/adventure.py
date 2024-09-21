c6d612067a141e7f1b70c7e8d6f28afb8469ccdd
Matthew Rosenthal <matthew2rosenthal@gmail.com>
2024-09-21 00:47:06 -0600

    loan

--- CSE110-Fall-2024/week3/adventure.py
+++ CSE110-Fall-2024/week3/adventure.py
@@ -2,7 +2,7 @@
 
 
 
-print('You enter a dark room and pull out of your pocket a [FLASHLIGHT] or a [GLOW STICK].')
+print('You enter a dark room in an old house and pull out of your pocket a [FLASHLIGHT] or a [GLOW STICK].')
 
 def choice('option1', 'option2'):
 
@@ -25,30 +25,30 @@ if decision1==1:
     print('You turn on the flashlight but it is dim because the batteries are dying but you can see some stairs that spiral going up too the floor [ABOVE] you and also [DOWN] to what looks like they are going into the basement')
     decision2=choice('ABOVE','BELOW')
     if decision2==1:
-        print('')
-        decision3=choice(,)
+        print('As you walk up the stairs you can smell the sent of old books and as you enter the room you see bookcases filled with books covering most of the walls except one with a fireplace with logs ready to be lit, you decide to[LIGHT] the fireplace or [EXPLORE] the room further')
+        decision3=choice('LIGHT','EXPLORE')
         if decision3==1:
             print('')
-            decision4=choice(,)
+            decision4=choice('','')
         elif decision3==2:
             print('')
-            decision4=choice(,)
+            decision4=choice('','')
     elif decision2==2:
         print('')
-        decision3=choice(,)
+        decision3=choice('','')
         
 elif decision1==2:
-    print('You crack the glow stick and soft dim light comes off the glow stick and notice something glowing on the wall that looks like a SWITCH and then you notice a spiral STAIR case going to the floor above and below you ')
+    print('You crack the glow stick and soft dim light comes off the glow stick and you notice something glowing on the wall that looks like a SWITCH and then you also notice a spiral STAIR case going to the floor above and below you ')
     decision2=choice('SWITCH','STAIRS')
     if decision2==1:
-            print('')
-            decision3=choice(,)
+        print('')
+        decision3=choice('','')
         if decision3==1:
             print('')
-            decision4=choice(,)
+            decision4=choice('','')
         elif decision3==2:
             print('')
-            decision4=choice(,)
+            decision4=choice('','')
     elif decision2==2:
             print('')
-            decision3=choice(,)
+            decision3=choice('','')
---
+++ CSE110-Fall-2024/week3/loan.py
@@ -0,0 +1,59 @@
+
+while True:
+    try:
+        size=int(input('How large is the loan? '))
+        if 1 <= size <= 10:
+            break
+        else:
+            print("Invalid input. Please enter a number between 1 and 10.")
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+
+while True:
+    try:
+        credit=int(input('How good is your credit history? '))
+        if 1 <= credit <= 10:
+            break
+        else:
+            print("Invalid input. Please enter a number between 1 and 10.")
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+        
+while True:
+    try:
+        income=int(input('How high is your income? '))
+        if 1 <= income <= 10:
+            break
+        else:
+            print("Invalid input. Please enter a number between 1 and 10.")
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+
+while True:
+    try:
+        down=int(input('How large is your down payment? '))
+        if 1 <= down <= 10:
+            break
+        else:
+            print("Invalid input. Please enter a number between 1 and 10.")
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+
+
+if size >= 5:
+    if credit>=7:
+        if income>=7:
+            print('yes')
+    elif credit>=7 or income>=7:
+        if down>=5:
+            print('yes')
+        else: print('no')
+else:
+    if credit<4:
+        print ('no')
+    elif income>=7 or down>=7:
+        print('yes')
+    elif income>=4 and down>=4:
+        print ('yes')
+    else: print ('no')
+

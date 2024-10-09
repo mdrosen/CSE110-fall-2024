05800100a05fd7a1d9081ae0943cfd2a2f5bb1a2
Matthew Rosenthal <matthew2rosenthal@gmail.com>
2024-10-09 15:55:57 -0600

    num game

--- CSE110-Fall-2024/week4/number_game.py
+++ CSE110-Fall-2024/week4/number_game.py
@@ -1,21 +1,24 @@
 import random
+while True:
+    number=random.randint(1,10)
+    print(number)
+    guess=0
+    run='yes'
+    while guess != number:
+        guess=int(input('what is your guess?: '))
 
+        if guess == number:
+            print('Congratulations, you guessed the right number.')
+            run=input('Do you want to play again (yes/no): ').lower()
 
-number=random.randint(1,10)
-#print(number)
-guess=0
+        elif guess < 1 or guess > 10:
+            print ('your number is not in the range of available numbers')
 
-while guess != number:
-    guess=int(input('what is your guess?: '))
+        elif guess < number:
+            print('Hint: The number is Higher.')
 
-    if guess == number:
-        print('Congraulations, you guessed the right number.')
-
-    elif guess < 1 or guess > 10:
-        print ('your number is not in the range of avalible numbers')
-
-    elif guess < number:
-        print('Hint: The number is Higher.')
-
-    elif guess > number:
-        print('Hint: The number is Lower.')
+        elif guess > number:
+            print('Hint: The number is Lower.')
+    if run == 'no':
+        break
+    

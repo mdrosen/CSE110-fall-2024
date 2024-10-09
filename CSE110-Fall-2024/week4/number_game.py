33b928de699fc4031d410a1f5c0da51ee7615870
Matthew Rosenthal <matthew2rosenthal@gmail.com>
2024-10-09 16:37:24 -0600

    formatting num game

--- CSE110-Fall-2024/week4/number_game.py
+++ CSE110-Fall-2024/week4/number_game.py
@@ -2,29 +2,29 @@ import random
 print('Welcome to the Number Guessing Game\n')
 print('Please pick a number between 1-100\n')
 while True:
-    number=random.randint(1,100)
+    number = random.randint(1,100)
     #print(number)
-    guess=0
-    run='yes'
-    times=0
+    guess = 0
+    run = 'yes'
+    times = 0
     while guess != number:
-        guess=int(input('what is your guess?: '))
+        guess = int(input('what is your guess?: '))
         times += 1
         if guess == number:
-            print('Congratulations, you guessed the right number.')
-            print((f'It took you {times} guesses'))
+            print('Congratulations, you guessed the right number.\n')
+            print((f'It took you {times} guesses\n'))
             
-            run=input('Do you want to play again (yes/no): ').lower()
+            run = input('Do you want to play again (yes/no): ').lower()
 
         elif guess < 1 or guess > 100:
-            print ('your number is not in the range of available numbers')
+            print ('your number is not in the range of available numbers\n')
 
         elif guess < number:
-            print('Hint: The number is Higher.')
+            print('Hint: The number is Higher.\n')
 
         elif guess > number:
-            print('Hint: The number is Lower.')
+            print('Hint: The number is Lower.\n')
     if run == 'no':
         break
-    elif run=='yes':
-        times=0
+    elif run =='yes':
+        times = 0

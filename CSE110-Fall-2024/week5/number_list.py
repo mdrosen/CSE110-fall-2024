16c22c7d55fe867f1b786950c8a62e03b676eebc
Matthew Rosenthal <matthew2rosenthal@gmail.com>
2024-10-16 17:12:51 -0600

    num list instructions

---
+++ CSE110-Fall-2024/week5/number_list.py
@@ -0,0 +1,28 @@
+'''Instructions
+Ask the user for a series of numbers, and append each one to a list. Stop when they enter 0.
+Once you have a list, have your program do the following:
+
+Core Requirements
+Work through these core requirements step-by-step to complete the program. Please don't skip ahead and do the whole thing at once, because others on your team may benefit from building the program up slowly.
+Compute the sum, or total, of the numbers in the list.
+Compute the average of the numbers in the list.
+Find the maximum, or largest, number in the list.
+The following shows the expected output:
+
+Enter a list of numbers, type 0 when finished.
+Enter number: 18
+Enter number: 36
+Enter number: 2
+Enter number: 48
+Enter number: 6
+Enter number: 12
+Enter number: 9
+Enter number: 0
+The sum is: 131
+The average is: 18.714285714285715
+The largest number is: 48
+Stretch Challenge
+Have the user enter both positive and negative numbers, then find the smallest positive number (the positive number that is closest to zero).
+Sort the numbers in the list and display the new, sorted list. Hint: There are python libraries that can help you here, try searching the internet for them.'''
+
+

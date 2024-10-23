e0a317d93c92e6c370a3902e2762d3e3e8516b6f
Matthew Rosenthal <matthew2rosenthal@gmail.com>
2024-10-23 13:37:16 -0600

    hr

--- CSE110-Fall-2024/week6/hr_system.py
+++ CSE110-Fall-2024/week6/hr_system.py
@@ -9,5 +9,7 @@ with open('BYU-I code\CSE110-Fall-2024\week6\hr_system.txt', 'r') as f:
         id_number = parts[1]
         job_title = parts[2]
         salary = float(parts[3])
+        
+        
         print(f'Name: {name} (ID: {id_number}),  Title: {job_title} - ${salary:.2f}')
         

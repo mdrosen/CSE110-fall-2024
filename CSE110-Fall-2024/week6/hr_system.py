99478641addee912e3befaeda61c11bef2a14d6f
Matthew Rosenthal <matthew2rosenthal@gmail.com>
2024-10-23 19:47:49 -0600

    Co-authored-by: FunAxel64 <FunAxel64@users.noreply.github.com> Co-authored-by: Ezra Casas <ezra-casas@users.noreply.github.com>

--- CSE110-Fall-2024/week6/hr_system.py
+++ CSE110-Fall-2024/week6/hr_system.py
@@ -8,8 +8,11 @@ with open('BYU-I code\CSE110-Fall-2024\week6\hr_system.txt', 'r') as f:
         name = parts[0]
         id_number = parts[1]
         job_title = parts[2]
-        salary = float(parts[3])
+        salary = float(parts[3]) 
+        paycheck = salary / 24 # Assuming they're paid twice a month.
         
+        if job_title == "Engineer":
+            paycheck += 1000
         
-        print(f'Name: {name} (ID: {id_number}),  Title: {job_title} - ${salary:.2f}')
-        
+        print(f'Name: {name} (ID: {id_number}),  Title: {job_title} - ${paycheck:.2f}')
+

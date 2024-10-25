2d235c5fbe5563fab0092f04bd16bd2827886a5c
Matthew Rosenthal <matthew2rosenthal@gmail.com>
2024-10-24 23:10:06 -0600

    wind chill frostbite update

--- CSE110-Fall-2024/week7/windchill.py
+++ CSE110-Fall-2024/week7/windchill.py
@@ -12,6 +12,21 @@ def F_to_C(F):
     C=(F - 32) / 1.8
     return C
 
+#Wind chill of 0 to -19°F: Frostbite possible in 30 minutes to exposed skin.
+#Wind chill of -20 to -39°F: Frostbite possible in 10 to 30 minutes.
+#Wind chill of -40 to -59°F: Frostbite possible in 5 to 10 minutes.
+#Wind chill of -60°F and below: Frostbite possible in less than 5 minutes.
+def frostbite_risk(w):
+    if 0 >= w >= -19:
+        return 'Frostbite possible in 30 minutes to exposed skin'
+    if -20 >= w >= -39:
+        return 'Frostbite possible in 10 to 30 minutes.'
+    if -40 >= w >=-59:
+        return 'Frostbite possible in 5 to 10 minutes'
+    if -60 >= w < -60:
+        return'Frostbite possible in less than 5 minutes.'
+
+
 def wind_chill(a , u):
     unit = u
     air_temp = a
@@ -32,11 +47,12 @@ def wind_chill(a , u):
         wind_speed_knots = wind_speed_mph * 0.5144
         wind_chill_F = 35.74 + (0.6215*air_temp_F) - (35.75*(wind_speed_mph**.16)) + (0.4275*air_temp_F*(wind_speed_mph**.16))
         wind_chill_C = 13.12 + (0.6215 * air_temp_C) - (11.37 * wind_speed__km_h**0.16) + (0.3965 * air_temp_C * (wind_speed__km_h**0.16))
-        
+        frostbite = frostbite_risk(wind_chill_F)
         print(f'The Wind Chill is: {wind_chill_F:.2f}{degree}F at a Air temp. of: {air_temp_F:.1f}{degree}F and Wind Speed of: {wind_speed_mph} MPH or a wind speed of: {wind_speed_knots:.2f} knots')
         print(f'The Wind Chill is: {wind_chill_C:.2f}{degree}C at a Air temp. of: {air_temp_C:.1f}{degree}C and Wind Speed of: {wind_speed__km_h:.2f} KM/H\n')
+        print(f'The risk of {frostbite} with a wind chill of: {wind_chill_F:.2f}{degree}F')
         print('---------------------------------------------------------------------------------------------------------------------------------')
-        
+
 def main():
     a = int(input('Air Temp: '))
     u = str(input(f'is the temp in {degree}F or {degree}C (C\F): ')).upper()
